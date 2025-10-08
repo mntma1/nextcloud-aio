f6a2d924c0c76d982ca05eecc82341ffab82d93f
Manfred N <114916491+mntma1@users.noreply.github.com>
2025-10-08 15:52:03 +0200

    Fehler in install_nc-aio.sh behoben

--- install_nc-aio.sh
+++ install_nc-aio.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Created by Manfred - 22.08.2025 16:23 Uhr
-echo Wo sollen die Benuzer-Dateien gespeichert werden(Da sollte genug Plattenplatz verfügbar sein)?
+echo 'Wo sollen die Benuzer-Dateien gespeichert werden(Da sollte genug Plattenplatz verfügbar sein)?'
 echo '(Mountpoint zu einer Partition oder einer SSD)'
 read -p 'Das Verzeichnis für die Benutze-Dateien bitte: ' UDIR 
 
@@ -15,8 +15,7 @@ docker run \
 --env APACHE_PORT=11000 \
 --env APACHE_IP_BINDING=0.0.0.0 \
 --env APACHE_ADDITIONAL_NETWORK="" \
---env SKIP_DOMAIN_VALIDATION=false \
---env NEXTCLOUD_DATADIR="$UDIR" \ # Für UserData: Hier sollte genug Speicherplatz zur Verfügung stehen!  
+--env NEXTCLOUD_DATADIR="$UDIR" \
 --volume nextcloud_aio_mastercontainer:/mnt/docker-aio-config \
 --volume /var/run/docker.sock:/var/run/docker.sock:ro \
 ghcr.io/nextcloud-releases/all-in-one:latest
